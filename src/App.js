260c065058422bf28098eb4c1f4bf010ebd74476
#Kali <brucaldan@gmail.com>
2023-08-26 00:29:54 +0800

    With second hero section in video but not working

--- src/App.js
+++ src/App.js
@@ -28,6 +28,42 @@ function App() {
           </div>
         </div>
       </div>
+
+      <section className="hero">
+        <div className="container">
+          <div className="hero__wrapper">
+            <div className="top__hero">
+              <p>
+                <span>Attention:</span> Employees, Students, Business Owners
+              </p>
+              <h1>
+                <span> We Have Uncovered A</span>
+                <br />
+                <span>'New Best Online Business'</span>
+              </h1>
+              <h2>That Almost Nobody Knows About..</h2>
+              <p>
+                That has the ability to earn your first P100,000 In less than 30
+                days without money, skills, and experience
+              </p>
+            </div>
+            <div className="mid__vid">
+              <img
+                src="https://static.wixstatic.com/media/2ea1d5_c3eb4a7839a24ed6b6b7857e3d8e862e~mv2.png/v1/fill/w_399,h_486,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2ea1d5_c3eb4a7839a24ed6b6b7857e3d8e862e~mv2.png"
+                alt=""
+              />
+              <img
+                src="https://static.wixstatic.com/media/2ea1d5_b74ffee136e845d7af431eb9c7cdc286f001.jpg/v1/fill/w_1005,h_562,al_c,q_85,usm_0.33_1.00_0.00,enc_auto/2ea1d5_b74ffee136e845d7af431eb9c7cdc286f001.jpg"
+                alt=""
+              />
+              <img
+                src="https://static.wixstatic.com/media/2ea1d5_4628a3e38fb34802bccbb7a5b0f1ffde~mv2.png/v1/fill/w_357,h_446,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2ea1d5_4628a3e38fb34802bccbb7a5b0f1ffde~mv2.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
