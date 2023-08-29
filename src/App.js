ef0c0c0a72ac119bf10a24229c20dace6955ea83
#Kali <brucaldan@gmail.com>
2023-08-29 18:24:08 +0800

    Until Call to action still not responsive

--- src/App.js
+++ src/App.js
@@ -81,10 +81,220 @@ function App() {
           <div className="left__link">
             <a href="#">Watch The Full MSB Masterclass On Youtube 100% FREE!</a>
             <button className="click-to-watch">CLICK HERE TO WATCH </button>
+            <img
+              src="https://static.wixstatic.com/media/2ea1d5_fa1b83a7490540a98661b23446d606ae~mv2.png/v1/fill/w_136,h_136,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2ea1d5_fa1b83a7490540a98661b23446d606ae~mv2.png"
+              alt=""
+            />
           </div>
           <div className="right__img">
             <img
               src="https://static.wixstatic.com/media/2ea1d5_83eb854eace64d03a9c4c2e4c1eb2250~mv2.jpg/v1/fill/w_489,h_233,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/maxresdefault.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="WUL">
+        <div className="container">
+          <div className="WUL__wrapper">
+            <h2>What You Will Learn Inside The Program</h2>
+
+            <div className="WUL__holder">
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_d676d0205ed84a9eb061549fe23cb211~mv2.gif"
+                    alt=""
+                  />
+                </div>
+                <h2>Tips and mindset before starting</h2>
+                <p>
+                  Having the right mindset for success is one of the most
+                  important aspects which will be their motivation for success.
+                  we will giving you advices, having the right mindset, and also
+                  breaking all beliefs that can't help you to succeed.{" "}
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_156ab350bcc84afab341b32b57a53975~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>How Does Multi Servicing Business Works</h2>
+                <p>
+                  In this Category, you will know how this works and will be
+                  able to earn you a significant amount of pay to supplement
+                  your current income. I will try my best to give you all the
+                  information to help you start working on this in the comfort
+                  of your own home
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_e587f36450524a77a402669387bc5f8c~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>How To Learn Skills Or Services</h2>
+                <p>
+                  In this Category, you will know how this works and will be
+                  able to earn you a significant amount of pay to supplement
+                  your current income. I will try my best to give you all the
+                  information to help you start working on this in the comfort
+                  of your own home
+                </p>
+              </div>
+            </div>
+
+            <div className="WUL__holder">
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_c564a77e5462421592bf5cc13c32abeb~mv2.gif"
+                    alt=""
+                  />
+                </div>
+                <h2>Top 5 Services We Offer</h2>
+                <p>
+                  ​In this video, when deciding which services to choose, most
+                  people go for the most popular fields. But, the most popular
+                  fields are not always the ones that pay the most. I will
+                  discuss the top 5 services offer that earn higher rates, you
+                  might want to explore less crowded fields where demand is
+                  high, and competition is less intense.{" "}
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_b4cea575aeaf4699b58c454eb89da21d~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>Ways To Offer Your Service The Best Way</h2>
+                <p>
+                  In this video, you will learn how to offer or deliver your
+                  great services the best way, we will give you ready to use
+                  templates so you can get the client asap. Watch until the end
+                  so that you won't miss any important information
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_1c12e0d310c24a06afe81a49edf09136~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>
+                  Finding Expert <br />
+                  Employees
+                </h2>
+                <p>
+                  Here you will learn how to find reliable employees and i will
+                  give you the exact scripts that we use to talk to an employee.
+                  we will also give a tutorial on how to create job ad with
+                  ready to use template like contracts, ads, caption and many
+                  more.
+                </p>
+              </div>
+            </div>
+
+            <div className="WUL__holder">
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_e53345ad31fc4e70afc8ce516da857b0~mv2.gif"
+                    alt=""
+                  />
+                </div>
+                <h2>
+                  Recommended Strategy To Find A Client For Direct Client?
+                </h2>
+                <p>
+                  Having the right mindset for success is one of the most
+                  important aspects which will be their motivation for success.
+                  we will giving you advices, having the right mindset, and also
+                  breaking all beliefs that can't help you to succeed.{" "}
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_415df2580a404a5280cf5777ce60384d~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>Best Techniques and Strategies To Get More Clients</h2>
+                <p>
+                  In this lesson you will learn the best techniques and
+                  strategies that we use to get more clients without doing a
+                  single thing. just copy and paste our formula. this pretty
+                  much guarantee you will have your 2nd client after you
+                  implement this
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_1bce493d74d0436fbe094495d8aa8f8a~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>How To Negotiate With A Client Regarding The Salary</h2>
+                <p>
+                  Here in this lesson you will learn what is the right way to
+                  negotiate your salary or your payment on your services, after
+                  thousands of meeting with clients and students we have now
+                  found the most successful way to get your desired salary or
+                  payment, with this private template
+                </p>
+              </div>
+            </div>
+
+            <div className="last__letter">
+              <h2>AND SO MUCH MORE</h2>
+              <p>The Lessons Above Are Only 9 Lessons Out</p>
+              <p>
+                Of <span>150+ Actionable Lessons</span> Inside The MSB Course
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="CTA">
+        <div className="CTA__wrapper">
+          <div className="left__link">
+            <h2>
+              Click The button Below To <br /> See The Complete Lessons <br />
+              Inside The MSB Course
+            </h2>
+            <button className="click-to-watch">CLICK IT HERE</button>
+            <img
+              src="https://static.wixstatic.com/media/2ea1d5_fa1b83a7490540a98661b23446d606ae~mv2.png/v1/fill/w_136,h_136,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2ea1d5_fa1b83a7490540a98661b23446d606ae~mv2.png"
+              alt=""
+            />
+          </div>
+          <div className="right__img">
+            <img
+              src="https://static.wixstatic.com/media/2ea1d5_2a81954f36a040b797ab0f57f697a83b~mv2.png/v1/fill/w_489,h_233,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Screenshot_6.png"
               alt=""
             />
           </div>
