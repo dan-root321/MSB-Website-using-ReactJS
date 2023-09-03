52e00d08c6b4a1648383cd7035e463fc3641e4de
#Kali <brucaldan@gmail.com>
2023-09-03 21:35:39 +0800

    until msb acadamy still not responsive

---
+++ src/App.js
@@ -0,0 +1,339 @@
+import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+function App() {
+  return (
+    <>
+      <div className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="logo">
+              <a href="#">
+                <img
+                  src="https://static.wixstatic.com/media/2ea1d5_14d60f6daf3f434e90954586d162a17b~mv2.png/v1/crop/x_0,y_173,w_1827,h_298/fill/w_223,h_36,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/MSB%20FINAL%20LOGO%20WHITE.png"
+                  alt=""
+                />
+              </a>
+            </div>
+
+            <div className="nav__item">
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">Testimonials</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section className="hero">
+        <div className="container">
+          <div className="hero__wrapper">
+            <div className="top__hero">
+              <p>
+                <span>Attention:</span> Employees, Students, Business Owners
+              </p>
+
+              <h1>
+                <span> We Have Uncovered A</span>
+                <br />
+                <span>'New Best Online Business'</span>
+              </h1>
+              <h2>That Almost Nobody Knows About..</h2>
+              <p>
+                That has the ability to earn your first P100,000 In less than 30
+                days without money, skills, and experience
+              </p>
+            </div>
+            <div className="mid__vid">
+              <img
+                src="https://static.wixstatic.com/media/2ea1d5_c3eb4a7839a24ed6b6b7857e3d8e862e~mv2.png/v1/fill/w_399,h_486,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2ea1d5_c3eb4a7839a24ed6b6b7857e3d8e862e~mv2.png"
+                alt=""
+              />
+              <img
+                src="https://static.wixstatic.com/media/2ea1d5_b74ffee136e845d7af431eb9c7cdc286f001.jpg/v1/fill/w_1005,h_562,al_c,q_85,usm_0.33_1.00_0.00,enc_auto/2ea1d5_b74ffee136e845d7af431eb9c7cdc286f001.jpg"
+                alt=""
+              />
+              <img
+                src="https://static.wixstatic.com/media/2ea1d5_4628a3e38fb34802bccbb7a5b0f1ffde~mv2.png/v1/fill/w_357,h_446,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2ea1d5_4628a3e38fb34802bccbb7a5b0f1ffde~mv2.png"
+                alt=""
+              />
+            </div>
+            <div className="get-started">
+              <div className="get-started__wrapper">
+                <img
+                  src="https://static.wixstatic.com/media/2ea1d5_75c4f647a69047879a8972e906eea4b0~mv2.png/v1/crop/x_158,y_60,w_1910,h_664/fill/w_952,h_331,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/BOOK%20.png"
+                  alt=""
+                />
+
+                <button className="enrollnow">Enroll now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="watch-here">
+        <div className="watch-here__wrapper">
+          <div className="left__link">
+            <a href="#">Watch The Full MSB Masterclass On Youtube 100% FREE!</a>
+            <button className="click-to-watch">CLICK HERE TO WATCH </button>
+            <img
+              src="https://static.wixstatic.com/media/2ea1d5_fa1b83a7490540a98661b23446d606ae~mv2.png/v1/fill/w_136,h_136,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2ea1d5_fa1b83a7490540a98661b23446d606ae~mv2.png"
+              alt=""
+            />
+          </div>
+          <div className="right__img">
+            <img
+              src="https://static.wixstatic.com/media/2ea1d5_83eb854eace64d03a9c4c2e4c1eb2250~mv2.jpg/v1/fill/w_489,h_233,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/maxresdefault.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
+      <section className="WUL">
+        <div className="container">
+          <div className="WUL__wrapper">
+            <h2>What You Will Learn Inside The Program</h2>
+
+            <div className="WUL__holder">
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_d676d0205ed84a9eb061549fe23cb211~mv2.gif"
+                    alt=""
+                  />
+                </div>
+                <h2>Tips and mindset before starting</h2>
+                <p>
+                  Having the right mindset for success is one of the most
+                  important aspects which will be their motivation for success.
+                  we will giving you advices, having the right mindset, and also
+                  breaking all beliefs that can't help you to succeed.{" "}
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_156ab350bcc84afab341b32b57a53975~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>How Does Multi Servicing Business Works</h2>
+                <p>
+                  In this Category, you will know how this works and will be
+                  able to earn you a significant amount of pay to supplement
+                  your current income. I will try my best to give you all the
+                  information to help you start working on this in the comfort
+                  of your own home
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_e587f36450524a77a402669387bc5f8c~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>How To Learn Skills Or Services</h2>
+                <p>
+                  In this Category, you will know how this works and will be
+                  able to earn you a significant amount of pay to supplement
+                  your current income. I will try my best to give you all the
+                  information to help you start working on this in the comfort
+                  of your own home
+                </p>
+              </div>
+            </div>
+
+            <div className="WUL__holder">
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_c564a77e5462421592bf5cc13c32abeb~mv2.gif"
+                    alt=""
+                  />
+                </div>
+                <h2>Top 5 Services We Offer</h2>
+                <p>
+                  ​In this video, when deciding which services to choose, most
+                  people go for the most popular fields. But, the most popular
+                  fields are not always the ones that pay the most. I will
+                  discuss the top 5 services offer that earn higher rates, you
+                  might want to explore less crowded fields where demand is
+                  high, and competition is less intense.{" "}
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_b4cea575aeaf4699b58c454eb89da21d~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>Ways To Offer Your Service The Best Way</h2>
+                <p>
+                  In this video, you will learn how to offer or deliver your
+                  great services the best way, we will give you ready to use
+                  templates so you can get the client asap. Watch until the end
+                  so that you won't miss any important information
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_1c12e0d310c24a06afe81a49edf09136~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>
+                  Finding Expert <br />
+                  Employees
+                </h2>
+                <p>
+                  Here you will learn how to find reliable employees and i will
+                  give you the exact scripts that we use to talk to an employee.
+                  we will also give a tutorial on how to create job ad with
+                  ready to use template like contracts, ads, caption and many
+                  more.
+                </p>
+              </div>
+            </div>
+
+            <div className="WUL__holder">
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_e53345ad31fc4e70afc8ce516da857b0~mv2.gif"
+                    alt=""
+                  />
+                </div>
+                <h2>
+                  Recommended Strategy To Find A Client For Direct Client?
+                </h2>
+                <p>
+                  Having the right mindset for success is one of the most
+                  important aspects which will be their motivation for success.
+                  we will giving you advices, having the right mindset, and also
+                  breaking all beliefs that can't help you to succeed.{" "}
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_415df2580a404a5280cf5777ce60384d~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>Best Techniques and Strategies To Get More Clients</h2>
+                <p>
+                  In this lesson you will learn the best techniques and
+                  strategies that we use to get more clients without doing a
+                  single thing. just copy and paste our formula. this pretty
+                  much guarantee you will have your 2nd client after you
+                  implement this
+                </p>
+              </div>
+
+              <div className="WUL__card">
+                <div className="image__holder">
+                  <img
+                    src="https://static.wixstatic.com/media/2ea1d5_1bce493d74d0436fbe094495d8aa8f8a~mv2.gif"
+                    alt=""
+                  />
+                </div>
+
+                <h2>How To Negotiate With A Client Regarding The Salary</h2>
+                <p>
+                  Here in this lesson you will learn what is the right way to
+                  negotiate your salary or your payment on your services, after
+                  thousands of meeting with clients and students we have now
+                  found the most successful way to get your desired salary or
+                  payment, with this private template
+                </p>
+              </div>
+            </div>
+
+            <div className="last__letter">
+              <h2>AND SO MUCH MORE</h2>
+              <p>The Lessons Above Are Only 9 Lessons Out</p>
+              <p>
+                Of <span>150+ Actionable Lessons</span> Inside The MSB Course
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="CTA">
+        <div className="CTA__wrapper">
+          <div className="left__link">
+            <h2>
+              Click The button Below To <br /> See The Complete Lessons <br />
+              Inside The MSB Course
+            </h2>
+            <button className="click-to-watch">CLICK IT HERE</button>
+            <img
+              src="https://static.wixstatic.com/media/2ea1d5_fa1b83a7490540a98661b23446d606ae~mv2.png/v1/fill/w_136,h_136,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2ea1d5_fa1b83a7490540a98661b23446d606ae~mv2.png"
+              alt=""
+            />
+          </div>
+          <div className="right__img">
+            <img
+              src="https://static.wixstatic.com/media/2ea1d5_2a81954f36a040b797ab0f57f697a83b~mv2.png/v1/fill/w_489,h_233,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Screenshot_6.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="MSB__academy">
+        <div className="container">
+          <div className="MSB__academy__wrapper">
+            <div className="top__msg">
+              <h2>
+                When You Join MSB Academy, <br /> You Get Full Immediate Access
+                To:
+              </h2>
+            </div>
+
+            <div className="MSB__bullet">
+              <div className="left__msg">
+                <h2>
+                  1. Exclusive MSB Course 2.0 Curriculum 150 Lessons Get Full
+                  Immediate Access To:
+                </h2>
+                <p>
+                  Over 50 hours of step by step training covering EVERY aspect
+                  of building an MSB business from scratch. This is almost a
+                  plug play system with enough success stories to back it up
+                  Signing clients, Finding 6 figure clients, building out your
+                  team, on boarding clients, invoicing, sales... this course has
+                  everything covered for you.
+                </p>
+              </div>
+              <div className="right__image">
+                <img
+                  src="https://static.wixstatic.com/media/2ea1d5_185acc63d5734434b8eb2334bacd99d3~mv2.png/v1/fill/w_487,h_231,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/mockup-platforms%20asda.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
